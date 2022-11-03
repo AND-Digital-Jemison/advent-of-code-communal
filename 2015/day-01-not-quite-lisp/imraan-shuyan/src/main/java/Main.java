66008618c2db67d33e296088503e7989847757b3
Imraan Sarwar <imraansarwar@X3N6JCQQ2T.local>
2022-11-03 14:54:12 +0000

    wrote unit tests to start to solve the problem

--- 2015/day-01-not-quite-lisp/imraan-shuyan/src/main/java/Main.java
+++ 2015/day-01-not-quite-lisp/imraan-shuyan/src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Imraan");
+        System.out.println("Hello World");
+        Main.Program("");
+    };
+
+    public static int Program(String input) {
+        if (input == "") {
+            return 0;
+        }
+        if (input == "(") {
+            return 1;
+        }
+        return 0;
     }
 }
--- 2015/day-01-not-quite-lisp/imraan-shuyan/src/test/java/TestTemplate.java
+++ 2015/day-01-not-quite-lisp/imraan-shuyan/src/test/java/TestTemplate.java
@@ -1,8 +1,20 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class TestTemplate {
     @Test
-    public void test() {
-        assert(true);
+    public void given_a_empty_string_returns_0() {
+        String input = "";
+        int result = Main.Program(input);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void given_a_single_open_bracket_returns_1() {
+        String input = "(";
+        int expected = 1;
+        int result = Main.Program(input);
+        assertEquals(expected, result);
     }
 }
