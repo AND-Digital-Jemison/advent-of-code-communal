10c8d42af9d4b581d53c714a8fb5cf7f5620215c
Elena Cavallero <elecav@hotmail.it>
2022-11-09 14:30:58 +0000

    finish first challenge

--- 2015/day-01-not-quite-lisp/jrimmer_ecavallero_rclark/src/main/java/Main.java
+++ 2015/day-01-not-quite-lisp/jrimmer_ecavallero_rclark/src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello world");
+    public static void main(String[] args) throws IOException {
+        List<String> input = getInput();
+        NotQuiteLisp notQuiteLisp = new NotQuiteLisp();
+        System.out.println(notQuiteLisp.getFloor(input.get(0)));
     }
+
+    private static List<String> getInput() throws IOException {
+        return Files.readAllLines(Paths.get("src/main/java/input.txt"));
+    }
+
+
+
 }
