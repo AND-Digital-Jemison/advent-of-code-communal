95769cdd04a4ca502d6e1753d7a4bfc9499cf862
Jack <jackrimmer2996@hotmail.co.uk>
2022-11-17 13:10:56 +0000

    Rename test

--- 2015/day-02-iwttwbnm/jrimmer/src/test/java/WrappingPaperTest.java
+++ 2015/day-02-iwttwbnm/jrimmer/src/test/java/WrappingPaperTest.java
@@ -1,8 +1,9 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class NotQuiteLispTest {
+public class WrappingPaperTest {
     WrappingPaperCalculator cal = new WrappingPaperCalculator();
+
     @Test
     public void calculateWrappingPaperNeeded_example1(){
         int r = cal.calculateWrappingPaperNeeded(new Dimensions(2,3,4));
@@ -14,5 +15,4 @@ public class NotQuiteLispTest {
         int r = cal.calculateWrappingPaperNeeded(new Dimensions(1,1,10));
         assertEquals(43, r);
     }
-
 }
